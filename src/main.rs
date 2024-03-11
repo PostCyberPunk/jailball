a6dc73ca5db721b1c4008691b2f449c553e16e9e
PostCyberPunk <lyhguest2@outlook.com>
2024-03-11 14:14:53 +0800

    ball init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .insert_resource(ClearColor(Color::BLACK))
+        // .insert_resource(ClearColor(Color::default().with_l(0.1)))
+        .run();
+}
+
+#[derive(Component)]
+struct Ball;
+
+fn setup(
+    mut cmd: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    cmd.spawn(Camera2dBundle::default());
+    cmd.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Circle::default()).into(),
+            material: materials.add(Color::WHITE),
+            transform: Transform::from_scale(Vec2::splat(20.).extend(1.)),
+            ..default()
+        },
+        Ball,
+    ));
 }
